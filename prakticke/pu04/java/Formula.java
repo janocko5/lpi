a98d3a480f6b5f9686851fb2b417270c8832060c
janocko5 <36777553+janocko5@users.noreply.github.com>
2019-05-04 16:58:14 +0200

    Riesenie 04
    Riesenie ulohy 04

--- prakticke/pu04/java/Formula.java
+++ prakticke/pu04/java/Formula.java
@@ -56,7 +56,7 @@ class Variable implements Formula {
 
     public Cnf toCnf() {
         /* TODO Implement this! */
-        return new Cnf();
+        return new Cnf(new Clause(new Literal(toString())));
     }
 }
 
@@ -113,17 +113,80 @@ class Negation extends CompositeFormula {
     public String toString() {
         return "-" + originalFormula().toString();
     }
+
+    @Override
+    public Cnf toCnf() {
+        Cnf podf = new Cnf();
+        podf.addAll(this.originalFormula().toCnf());
+        try{
+            podf.remove(0);
+        }
+        catch(Exception e){
+
+        }
+
+        podf.add(new Clause(new Literal(toString()), new Literal(originalFormula().toString())));
+        podf.add(new Clause(Literal.Not(originalFormula().toString()), Literal.Not(toString())));
+
+        podf.add(0, new Clause(new Literal(toString())));
+        return podf;
+    }
 }
 
 class Conjunction extends CompositeFormula {
     public Conjunction(Formula[] formulas) {
         super(formulas, "&");
     }
+
+    @Override
+    public Cnf toCnf() {
+        Cnf podf = new Cnf();
+        try{
+            podf.remove(0);
+        }
+        catch(Exception e){
+
+        }
+        Clause cl = new Clause(new Literal(toString()));
+        for (Formula f: this.subf()) {
+            podf.addAll(f.toCnf());
+            podf.add(new Clause(Literal.Not(toString()), new Literal(f.toString())));
+            cl.add(Literal.Not(f.toString()));
+        }
+        podf.add(cl);
+        podf.add(0, new Clause(new Literal(toString())));
+
+        return podf;
+
+    }
 }
 
 class Disjunction extends CompositeFormula {
     public Disjunction(Formula[] formulas) {
         super(formulas, "|");
+    }
+
+    @Override
+    public Cnf toCnf() {
+        Cnf podf = new Cnf();
+        try{
+            podf.remove(0);
+        }
+        catch(Exception e){
+
+        }
+
+        if(subf().length == 0) { return new Cnf(new Clause(Literal.Not(toString()))); }
+        Clause cl = new Clause(Literal.Not(toString()));
+        for (Formula f: this.subf()) {
+            podf.addAll(f.toCnf());
+            podf.add(new Clause(Literal.Not(toString()), new Literal(f.toString())));
+            cl.add(new Literal(f.toString()));
+        }
+        podf.add(cl);
+        podf.add(0, new Clause(new Literal(toString())));
+
+        return podf;
     }
 }
 
@@ -137,16 +200,57 @@ class BinaryFormula extends CompositeFormula {
     public Formula rightSide() {
         return subf()[1];
     }
+
+    @Override
+    public Cnf toCnf() {
+        return super.toCnf();
+    }
 }
 
 class Implication extends BinaryFormula {
     public Implication(Formula a, Formula b) {
         super(a, b, "->");
     }
+
+    @Override
+    public Cnf toCnf() {
+        Cnf podf = new Cnf();
+        podf.addAll(leftSide().toCnf());
+        try{
+            podf.remove(0);
+        }
+        catch(Exception e){
+
+        }
+        podf.addAll(rightSide().toCnf());
+        try{
+            podf.remove(0);
+        }
+        catch(Exception e){
+
+        }
+
+        podf.add(new Clause(Literal.Not(toString()), Literal.Not(leftSide().toString()), new Literal(rightSide().toString())));
+        podf.add(new Clause(Literal.Not(rightSide().toString()), new Literal(toString())));
+        podf.add(new Clause(new Literal(leftSide().toString()), new Literal(toString())));
+        podf.add(0, new Clause(new Literal(toString())));
+
+        return podf;
+    }
 }
 
 class Equivalence extends BinaryFormula {
     public Equivalence(Formula a, Formula b) {
         super(a, b, "<->");
+    }
+
+    @Override
+    public Cnf toCnf() {
+        Implication prva = new Implication(leftSide(), rightSide());
+        Implication druha = new Implication(rightSide(), leftSide());
+
+        Formula[] formulas = {prva, druha};
+        Conjunction spolu = new Conjunction(formulas);
+        return spolu.toCnf();
     }
 }
